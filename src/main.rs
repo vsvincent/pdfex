c8b41315face55394fbf43eb2508e3795bec47db
vsvincent <vincentvs01@yahoo.com>
2024-11-16 12:39:05 +0100

    feat: add jpg file saving

--- src/main.rs
+++ src/main.rs
@@ -10,35 +10,45 @@ use structopt::StructOpt;
     about = "If this very complex tool is confounding you, use `-h`"
 )]
 struct Opt {
-    path: String,
+    save_dir: String,
+    file_path: String,
 }
 
 fn main() -> Result<(), PDF2ImageError> {
     let opt = Opt::from_args();
-    println!("Loading PDF... {}", opt.path);
+    println!("Loading PDF... {}", opt.file_path);
 
-    if !fs::metadata(&opt.path).is_ok() {
-        eprintln!("Error: File not found at path: {}", opt.path);
+    if !fs::metadata(&opt.file_path).is_ok() {
+        eprintln!("Error: File not found at path: {}", opt.file_path);
         return Err(PDF2ImageError::Io(std::io::Error::new(
             std::io::ErrorKind::NotFound,
-            format!("File not found `{}`", opt.path),
+            format!("File not found `{}`", opt.file_path),
         )));
     } else {
-        println!("File found at path: {}", opt.path);
+        println!("File found at path: {}", opt.file_path);
     }
     utils::print::print_paths_in_current_dir().unwrap();
-    let pdf = match PDF::from_file(opt.path) {
+    let pdf = match PDF::from_file(opt.file_path) {
         Ok(pdf) => pdf,
         Err(e) => {
             eprintln!("Failed to load PDF: {}", e);
             return Err(e);
         }
     };
-    let pages = pdf.render(
-        pdf2image::Pages::Range(1..=8),
-        RenderOptionsBuilder::default().build()?,
-    );
-    println!("{:?}", pages.unwrap().len());
-
+    let pages = pdf
+        .render(
+            pdf2image::Pages::Range(1..=8),
+            RenderOptionsBuilder::default().build()?,
+        )
+        .unwrap();
+    println!("{:?}", pages.len());
+    let pages_enumerable = pages.iter().enumerate();
+    for (i, page) in pages_enumerable {
+        let file_name = format!("{}page_{}.jpg", opt.save_dir, i + 1);
+        match page.save(&file_name) {
+            Ok(_) => println!("Saved {}", file_name),
+            Err(e) => eprintln!("Failed to save {}: {}", file_name, e),
+        }
+    }
     Ok(())
 }
